8f6c21b7d151f46c51ddc83ea067f15b9483df80
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2026-06-08 12:26:55 +0100

    Added PyTorch network with MNIST digit dataset training functionality

---
+++ training/main.py
@@ -0,0 +1,57 @@
+# Use PyTorch and network.py to train a FNN on the MNIST dataset
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import v2
+
+from network import FullyConnectedNetwork
+
+def train_model():
+    # train model on MNIST dataset
+    training_data = datasets.MNIST(
+        root="dataset_data",
+        train=True,
+        download=True,
+        transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+    )
+    test_data = datasets.MNIST(
+        root="dataset_data",
+        train=False,
+        download=True,
+        transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+    )
+
+    # Hyperparameter setup
+    batch_size = 64
+    learning_rate = 1e-3
+    epochs = 20
+
+    # Set up dataloaders, model, loss function and optimizer
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    model = FullyConnectedNetwork([28*28, 512, 256, 10])
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    # train model
+    for t in range(epochs):
+        print(f"===== Epoch {t+1} =====")
+        model.train_loop(train_dataloader, loss_fn, optimizer, 100)
+        model.test_loop(test_dataloader, loss_fn)
+        print("")
+    print("===== Epochs Done =====")
+
+    # TODO Add model parameter saving
+
+def run_model():
+    # run pre-trained MNIST dataset model
+    pass
+
+
+# TODO Here, add a reasonable main, switching between train and run mode, export/import weights
+
+if __name__ == "__main__":
+    train_model()
+
---
+++ training/network.py
@@ -0,0 +1,81 @@
+from collections import OrderedDict
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.optim.optimizer import Optimizer
+from torch.nn.modules.loss import _Loss
+
+
+class FullyConnectedNetwork(nn.Module):
+    """Fully Connected Network, ReLU activation on hidden layers"""
+
+    def __init__(self, layers: list):
+        """layers: list of numbers corresponding to layers and their neuron counts"""
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.layers = nn.Sequential(
+            OrderedDict(self._get_layer_list(layers))
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.layers(x)
+        return logits
+
+    def train_loop(self, dataloader: DataLoader, loss_fn: _Loss, optimizer: Optimizer, print_interval: int = None):
+        print("--- training loop ---")
+        dataset_size = len(dataloader.dataset)
+        batch_size = dataloader.batch_size
+        # set model to train mode
+        self.train()
+
+        for batch, (X, y) in enumerate(dataloader):
+            # Compute loss and prediction with forward pass computation graph
+            prediction = self(X)
+            loss = loss_fn(prediction, y)
+
+            # Traverse computation graph of forward pass backwards to accumulate loss gradients
+            loss.backward()
+            # Use calculated gradients to perform a single optimizer step
+            optimizer.step()
+            # Reset gradients for next training step
+            optimizer.zero_grad()
+
+            if print_interval is not None:
+                if batch % print_interval == 0:
+                    loss, current = loss.item(), batch * batch_size + len(X)
+                    print(f"loss: {loss:>7f} [{current:>5d}/{dataset_size:>5d}]")
+
+        print("--- training loop done ---")
+
+    def test_loop(self, dataloader: DataLoader, loss_fn: _Loss):
+        print("--- test loop ---")
+        dataset_size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+        # set model to eval mode
+        self.eval()
+
+        # evaluate without computing gradients/computation graph to save memory
+        with torch.no_grad():
+            for X, y in dataloader:
+                prediction = self(X)
+                test_loss += loss_fn(prediction, y).item()
+                correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
+
+        test_loss /= num_batches
+        correct /= dataset_size
+        print(f"Test Result:\n -> accuracy: {100*correct}\n -> average loss: {test_loss}")
+        print("--- test loop done ---")
+
+
+    def _get_layer_list(self, layers: list):
+        # Initial layer
+        result = [("fc0", nn.Linear(layers[0], layers[1]))]
+        # Append ReLU for previous layer and next layer to leave final (output) layer without ReLU
+        for i in range(1, len(layers) - 1):
+            result.append((f"relu{i}", nn.ReLU()))
+            result.append((f"fc{i}", nn.Linear(layers[i], layers[i+1])))
+        return result
+
